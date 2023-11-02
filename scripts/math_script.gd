d7a172d4c0cc6e043b2ae068f6bdbe9bbf0a5ddf
Kadir Daniel Arslan <kadirdanielarslan@gmx.de>
2023-11-02 18:49:26 +0100

    Testat 2 Final, optimized

--- scripts/math_script.gd
+++ scripts/math_script.gd
@@ -1,12 +1,14 @@
 extends Node
 
 # Integers
-var integer_a = 5
-var integer_b = 2
-var float_a = 5.0
-var float_b = 2.0
-var evaluation_integer = 27
-var is_counting := true
+var integer_a: int = 5
+var integer_b := 2
+var float_a: float = 5.0
+var float_b := 2.0
+var a 
+var b 
+var evaluation_integer: int = 27
+var is_counting := false
 var x = 0
 
 # Called when the node enters the scene tree for the first time.
@@ -16,22 +18,22 @@ func _ready():
 	else:
 		print("odd")
 		
-	print(integer_a + integer_b)
-	print(integer_a - integer_b)
-	print(integer_a * integer_b)
-	print(integer_a / integer_b) #Dezimal wird abgerundet
+	_math(integer_a, integer_b)		#Divisionsergebnis abgerundet
+	_math(float_a, float_b)		#Divisionsergebnis genau
 	
-	print(float_a + float_b)
-	print(float_a - float_b)
-	print(integer_a * float_b)
-	print(float_a / float_b) #Dezimal wird akurat ausgegegben
 	while is_counting == true:
 		x += 1
 		print(x)
-	if evaluation_integer == false:
+	if is_counting == false:
 		x = 0
 	pass # Replace with function body.
 
+func _math(a, b):
+		print(a + b)
+		print(a - b)
+		print(a * b)
+		print(a / b)
+		pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
