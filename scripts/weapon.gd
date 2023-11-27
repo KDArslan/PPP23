65e6955beb653972e0004f5904378fa125d15825
Kadir Daniel Arslan <kadirdanielarslan@gmx.de>
2023-11-27 14:53:12 +0100

    Testat 05/ Dauerschießen mit Feuerrate

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -2,11 +2,13 @@ extends Node2D
 
 @export var projectile_scene : PackedScene
 @export var projectile_speed : float = 500.0
+@export var projectile_firerate : float = 10
 var aim_direction : Vector2 = Vector2.RIGHT 
+@onready var timer := $Timer
 
-
-func _input(event : InputEvent):
-	if event.is_action_pressed("shoot"): 
+func _physics_process(delta):
+	if Input.is_action_pressed("shoot") and timer.is_stopped(): 
+		timer.wait_time = 1/projectile_firerate
 		#Node aus PackedScene erzeugen 
 		var projectile = projectile_scene.instantiate()
 		get_tree().root.add_child(projectile)
@@ -16,6 +18,7 @@ func _input(event : InputEvent):
 		#setzten der Richtung und der Geschwindigkeit der Projektile
 		projectile = projectile as Projectile
 		projectile._initialize(projectile_speed, aim_direction)
+		timer.start()
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
