63b435ed9c36aad8af993808e9efe8bf83e2a44f
Kadir Daniel Arslan <kadir.arslan@uni-bayreuth.de>
2023-12-06 13:29:03 +0100

    Health bar added

--- scripts/character_controller.gd
+++ scripts/character_controller.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 class_name Avatar
 
+signal health_changed(current: int, max : int)
+
 @export var speed : float = 100.0
-var hit_points = 5
+@export var max_health = 5
+var current_health = max_health
+
+func _ready():
+	health_changed.emit(current_health, max_health)
 
 func _physics_process(_delta):
 	var input_value = Input.get_vector("left", "right", "up", "down")
@@ -17,6 +23,7 @@ func _physics_process(_delta):
 
 func take_hit():
 	print("Auaa")
-	hit_points -= 1
-	if hit_points == 0:
+	current_health -= 1
+	health_changed.emit(current_health, max_health)
+	if current_health <= 0:
 		queue_free()
---
+++ scripts/health_bar.gd
@@ -0,0 +1,6 @@
+extends ProgressBar
+
+func _on_avatar_health_changed(current, max):
+	max_value = max 
+	value = current
+	
