f59c3d9742208b644f0144830e93e938753f71e4
Kadir Daniel Arslan <kadir.arslan@uni-bayreuth.de>
2023-12-06 13:51:32 +0100

    Spawn path changes

--- scripts/enemy_spawn_path.gd
+++ scripts/enemy_spawn_path.gd
@@ -1,17 +1,19 @@
-extends Path2D
+extends PathFollow2D
 
 @onready var spawn_timer : Timer = $Timer
 @export var enemy_scene : PackedScene
 @export var spawn_frequency : float = 5.0
-var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 #Add path follow to spawn at entirely random point along path follow progress
 #Currently spawning at defined points
 
 func _on_timer_timeout():
 	spawn_timer.wait_time = spawn_frequency
-	var rand = rng.randi_range(0, curve.point_count -1)
-	var new_pos = curve.get_point_position(rand)
-	#print("Enemy spawned at pos: (" + str(new_pos.x) + ", " + str(new_pos.y) + ")")
+	var rng : RandomNumberGenerator = RandomNumberGenerator.new()
+	var spawn_point = rng.randf_range(0.0, 1.0)
+	progress_ratio = spawn_point
+	#var new_pos = get_progress_ratio()
+	#print("Enemy spawned at pos: (" + str(spawn_point.x) + ", " + str(spawn_point.y) + ")")
+	#spawn enemy
 	var new_enemy = enemy_scene.instantiate()
-	new_enemy.position = new_pos
+	new_enemy.position = global_position
 	get_tree().root.add_child(new_enemy)
