7bdeec9858fac7c7f90071b2df93ba9930f0f169
Kadir Daniel Arslan <kadir.arslan@uni-bayreuth.de>
2024-01-10 12:54:33 +0100

    first audio effects, shoot and spawn

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -6,6 +6,8 @@ extends Node2D
 var aim_direction : Vector2 = Vector2.RIGHT 
 @onready var firerate_timer : Timer = $Timer
 
+signal on_shoot()
+
 func _physics_process(_delta):
 	aim_direction = get_global_mouse_position() - global_position
 	aim_direction.normalized()
@@ -14,6 +16,7 @@ func _physics_process(_delta):
 			firerate_timer.wait_time = 1/projectile_firerate
 		else:
 			firerate_timer.wait_time = 0
+		_on_on_shoot()
 		#Node aus PackedScene erzeugen 
 		var projectile = projectile_scene.instantiate()
 		get_tree().root.add_child(projectile)
@@ -22,3 +25,9 @@ func _physics_process(_delta):
 		projectile._initialize(projectile_speed, aim_direction)
 		#Reset Timer
 		firerate_timer.start()
+
+
+func _on_on_shoot():
+	var shoot_sound = $ShootSound2D
+	shoot_sound.play()
+	
