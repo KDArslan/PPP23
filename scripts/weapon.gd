c38bb083528ef1384ebf2bf974137aab5a5a3c97
Kadir Daniel Arslan <kadirdanielarslan@gmx.de>
2024-02-05 17:39:56 +0100

    pistol + shotgun weapon Resources and weapon switching

--- scripts/weapon.gd
+++ scripts/weapon.gd
@@ -3,12 +3,29 @@ extends Node2D
 @export var projectile_scene : PackedScene
 @export var projectile_speed : float = 500.0
 @export var projectile_firerate : float = 10
+
+@onready var equipped_weapon : String = "pistol"
+@export var pistol : Resource
+@export var shotgun : Resource
+
 var aim_direction : Vector2 = Vector2.RIGHT 
 @onready var firerate_timer : Timer = $Timer
 
 signal shot()
 
+func _ready():
+	projectile_speed = pistol.projectile_speed
+
 func _physics_process(_delta):
+	#if Input.is_action_just_pressed("switch_weapon"):
+		#print("switch")
+	if Input.is_action_just_pressed("switch_weapon") and equipped_weapon == "pistol":
+		equipped_weapon = "shotgun"
+		projectile_speed = shotgun.projectile_speed
+	elif Input.is_action_just_pressed("switch_weapon") and equipped_weapon == "shotgun":
+		equipped_weapon = "pistol"
+		projectile_speed = pistol.projectile_speed
+
 	aim_direction = get_global_mouse_position() - global_position
 	aim_direction.normalized()
 	if Input.is_action_pressed("shoot") and firerate_timer.is_stopped() and Global.current_time > 0: 
@@ -22,6 +39,6 @@ func _physics_process(_delta):
 		get_tree().current_scene.add_child(projectile)
 		projectile.position = global_position
 		#setzten der Richtung und der Geschwindigkeit der Projektile
-		projectile._initialize(projectile_speed, aim_direction)
+		projectile._initialize(projectile_speed , aim_direction)
 		#Reset Timer
 		firerate_timer.start()
---
+++ scripts/weapon_resource.gd
@@ -0,0 +1,13 @@
+extends Resource
+class_name WeaponResource
+
+@export var burst_amount : int
+@export var size : float
+@export var projectile_speed : float 
+@export var spread_cone_angle : Vector2
+
+#● Vector3 rotated(axis: Vector3, angle: float) gibt den Vektor, um eine Angabe im
+#Bogenmaß rotiert, zurück.
+#● deg_to_rad(value : float) rechnet Gradangaben in Bogenmaß um
+#● Bsp: var new_direction =
+#initial_direction.rotated(deg_to_rad(spread_angle_degrees))
